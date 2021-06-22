c909f05de33bef8bd8f8b2537503ee545b0bf952
damonlin1 <79048358+damonlin1@users.noreply.github.com>
2021-06-21 21:46:18 -0400

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -9,30 +9,30 @@ TEXT_SENSITIVITY = 0.011
 def split_images(src_dir, dest_dir):
     """
     Split images in the source directory into left and right halves, convert them
-    into pdf files, and saves them to the destination directory with title 'imgXXX.pdf'.
+    into pdf files, and saves them to the destination directory.
+
     :param src_dir: the source directory path
     :param dest_dir: the destination directory path
     :return: none
     """
-    image_num = 1
-    for folder in os.listdir(src_dir):
+    for folder in tqdm(os.listdir(src_dir)):
         if folder != '.DS_Store':  # Ignore .DS_Store because it is not a folder
             for file in os.listdir(f'{src_dir}/{folder}'):
                 # Ignore duplicated blurry images with 'vignvis' prefix
-                if file.endswith('jpg') and not file.startswith('vignvis'):
+                if file.lower().endswith('jpg') and not file.startswith('vignvis'):
                     image = Image.open(f'{src_dir}/{folder}/{file}')
                     width, height = image.size
                     image_left = image.crop((0, 0, 0.38 * width, height))
                     image_right = image.crop((0.62 * width, 0, width, height))
-                    image_left.save(f'{dest_dir}/image{image_num}.png')
-                    image_right.save(f'{dest_dir}/image{image_num + 1}.png')
-                    image_num += 2
+                    image_left.save(f'{dest_dir}/{folder}/{file[0:len(file) - 4]}_l.png')
+                    image_right.save(f'{dest_dir}/{folder}/{file[0:len(file) - 4]}_r.png')
 
 
 def extract_text(img_folder, txt_folder):
     """
     Extracts text from images in the image folder and writes into text files of the
     text folder using batch processing with AWS Textract.
+
     :param img_folder: the folder of images
     :param txt_folder: the folder of text files
     :return: none
@@ -47,7 +47,7 @@ def extract_text(img_folder, txt_folder):
 
             response = client.detect_document_text(Document={'Bytes': bytes_img})
 
-            with open(f'{txt_folder}/{img[0:len(img) - 4]}.txt', 'w') as txt_file:
+            with open(f'{txt_folder}/{img[:-4]}.txt', 'w') as txt_file:
                 for entry in response['Blocks']:
                     if entry['BlockType'] != 'PAGE':
                         txt_file.write(entry['Text'] + ' ')
