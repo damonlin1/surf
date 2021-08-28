4fdd90d79cd280cf8a3b86df2656296d320afaad
damonlin1 <79048358+damonlin1@users.noreply.github.com>
2021-08-28 09:19:16 -0400

    Update image_processing.py

--- image_processing.py
+++ image_processing.py
@@ -1,7 +1,20 @@
-from PIL import Image
+from difflib import SequenceMatcher
+from PIL import Image, ImageDraw, ImageEnhance, ImageFont
 from tqdm import tqdm
 import boto3
+import csv
+import io
+import numpy as np
 import os
+import unidecode
+
+
+break_phrases = ['est décédé', 'est décédée', 'constate le décès', 'est accouche', 'est accouchee', 'fils de',
+                 'fille de', 'Divorcée', 'Divorcé', 'Veuve de', 'Veuf de', 'Célibataire', 'transcrit', 'domiciliée a',
+                 'domicilié a', ' ne a ', ' né au ', ' née a ', ' née au ', 'Epoux de ', 'Epouse de ', 'Dressé',
+                 'Unsplit']
+date_break_phrases = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre',
+                      'novembre', 'decembre', 'en mil huit', 'en mil neuf']
 
 PAGE_SENSITIVITY = 0.30
 TEXT_SENSITIVITY = 0.011
@@ -189,6 +202,142 @@ def smart_crop(filename):
 
     cropped_img = img.crop((x1 - 20, 0, x2 + 25, height))
     cropped_img.save(f'1972_edited/{filename}_smart_cropped.png')
+
+
+def is_close(num, num_lst):
+    """
+    Checks if a number is close (within 10) to any of the numbers in the list.
+
+    :param num: the number to check
+    :param num_lst: the list to check against
+    :return: true if the number is within 10 to any number in the list, false otherwise
+    """
+    for number in num_lst:
+        if abs(num - number) < 10:
+            return True
+    return False
+
+
+def parse_data(src_file, dest_file):
+    """
+    Parses the information of the data into place of birth, date of birth, occupation,
+    parent information, place of death, and date of death.
+
+    :param src_file: the text file of the original data
+    :param dest_file: the text file of comma delimited parsed data
+    :return: none
+    """
+    # First normalize all phrases by removing accents and lowercase
+    processed_break_phrases = []
+    for phrase in break_phrases:
+        processed_break_phrases.append(unidecode.unidecode(phrase).lower())
+
+    # Open the source and destination files
+    with io.open(src_file, 'r', encoding='utf-8') as f:
+        with io.open(dest_file, 'w', encoding='utf-8') as g:
+            # Clean up messy data from AWS Textract
+            line = f.readline().replace('*', '').replace('-', '').replace('=', '')
+            while line:
+                date_of_birth_marked = False
+                # If it is not an empty line, split the act
+                if line != '\n':
+                    # Keeps track of where to split and the splitting identifier
+                    markings = {0: '<<Date of death>>: '}
+                    # Keeps track of the places where spaces occur
+                    space_idx = []
+                    # Identify the act label
+                    if line.startswith('Unsplit'):
+                        markings[0] = '<<Act label>>: '
+
+                    # Loops through the characters one by one
+                    for i in range(len(line) - 13):
+                        if line[i] == ' ':
+                            space_idx.append(i)
+
+                        # Checks if the words in the act matches any of the break phrases
+                        for word in processed_break_phrases:
+                            # Normalize the word being compared in the act
+                            temp = unidecode.unidecode(line[i:i + len(word)]).lower()
+                            if SequenceMatcher(None, temp, word).ratio() > 0.9:
+                                # One character shifts may result in the word getting recognized multiple times.
+                                # This ensures that once a break signal is added, no more is added nearby.
+                                if not is_close(i, list(markings.keys())):
+                                    # The following are the list of splitting identifiers
+                                    if word in ['est decede', 'est decedee', 'constate le deces', 'est accouche',
+                                                'est accouchee']:
+                                        markings[i] = '<<Death>>: '
+                                    if word in ['fils de', 'fille de']:
+                                        markings[i] = '<<Parents>>: '
+                                    if word in ['divorcee', 'divorce', 'veuve de', 'veuf de', 'celibataire',
+                                                'epoux de ', 'epouse de ']:
+                                        markings[i] = '<<Relationship>>: '
+                                    if word == 'transcrit':
+                                        markings[i] = '<<Transcription>>: '
+                                    if word in [' ne a ', ' ne au ', ' nee a ', ' nee au ']:
+                                        markings[i] = '<<Birth>>: '
+                                    if word == 'dresse':
+                                        markings[i] = '<<Misc>>: '
+
+                        # Do the same thing with finding if word match a date
+                        for word in date_break_phrases:
+                            temp = unidecode.unidecode(line[i:i + len(word)]).lower()
+                            if SequenceMatcher(None, temp, word).ratio() > 0.8:
+                                if not is_close(i, list(markings.keys())):
+                                    try:
+                                        idx = space_idx[len(space_idx) - 3]
+                                    except IndexError:
+                                        pass
+                                    if not is_close(idx, list(markings.keys())):
+                                        # Second occurrence is date of birth
+                                        if 0.1 < i / len(line) < 0.7 and not date_of_birth_marked:
+                                            markings[idx] = '<<Date of birth>>: '
+                                            date_of_birth_marked = True
+
+                    # Get the index of the places to split
+                    markings_list = sorted(list(markings.keys()))
+                    print(markings)
+                    # Write the data into the destination text file
+                    for i in range(len(markings_list) - 1):
+                        g.write(unidecode.unidecode(markings[markings_list[i]] +
+                                line[markings_list[i]:markings_list[i + 1]] + '\n'))
+                    g.write(unidecode.unidecode(markings[markings_list[len(markings_list) - 1]] +
+                                                line[markings_list[len(markings_list) - 1]:] + '\n'))
+
+                # Continue with the next act
+                line = f.readline().replace('*', '').replace('-', '').replace('=', '')
+
+
+def create_csv(txt_file, csv_file, keywords):
+    """
+    Creates a csv file from a text file.
+
+    :param txt_file: the name of the text file to convert into a csv file
+    :param csv_file: the name of the csv file
+    :param keywords:
+    :return: none
+    """
+    problem_files = []
+    with open(csv_file, 'w') as dest:
+        info = csv.writer(dest, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        info.writerow(['<<Act label>>'] + keywords)
+        with open(txt_file, 'r') as src:
+            line = src.readline()
+            row = [''] * (len(keywords) + 1)
+            while line:
+                for j, keyword in enumerate(keywords):
+                    if line.startswith(keyword):
+                        idx = line.index(':')
+                        row[j + 1] = line[idx + 1:]
+                if line.startswith('<<Act label>>'):
+                    idx = line.index(':')
+                    row[0] = line[idx + 1:]
+                    if '' in row:
+                        problem_files.append(line)
+                    info.writerow(row)
+                    row = [''] * (len(keywords) + 1)
+                line = src.readline()
+    print(sorted(problem_files))
+    print(len(problem_files))
 
 
 if __name__ == '__main__':
